e188a153c24250ba0b1512f5e0a9064514f65265
AVI CRAIMER <AVI.CRAIMER@cbc.ca>
2019-07-19 17:04:56 -0400

    Map values deep

--- src/utils/transmutation.js
+++ src/utils/transmutation.js
@@ -1,60 +1,55 @@
 const _ = require('lodash');
-
+const {isBasicData} = require('./introspection')
 //These functions should never be exposed outside the PAO module.
-exports.mapValuesDeep = (obj, callback) => {
-    if (Array.isArray(obj)) {
-      return obj.map(innerObj => mapValuesDeep(innerObj, callback));
-    } else if (_.isObject(obj)) {
-      return _.mapValues(obj, val => mapValuesDeep(val, callback));
-    } else {
-      return callback(obj);
-    }
-  };
 
 
 
-exports.getEntity = function (proxyObj) {
-    return proxiesToEntitiesMap.get(proxyObj);
-}
 
-exports.transmuteToEntity = function (unknown) {
-    if (Array.isArray(unknown)) {
-        return unknown.map(item => {
-            //Get the original tree item and replace the proxy with it
-            if (isProxy(value)) {
-                return getEntity(item);
-            } else {
-                return item;
-            }
-        });
-    } else if (Object.prototype.toString.call(unknown) === "[object Object]" ||Object.prototype.toString.call(unknown) === "[object Map]") {//This determines that it is a regular object or a Map, not an array or function or set etc
-        newObj = {};
-        for (const key in object) {
-            if (object.hasOwnProperty(key)) {
-                const value = object[key];
-                if (isProxy(value)) {
-                    newObj[key] = getEntity(value);
-                } else {
-                    newObj[key] = value;
-                }
-            }
-        }
-    }
-
-    else {
-        return item;
-    }
+// mapValuesDeep = (value, callback) => {
+//     if (Array.isArray(value)) {
+//       return value.map(innerValue => mapValuesDeep(innerValue, callback));
+//     } else if (_.isObject(value)) {
+//       return   _.mapValues(value, val => mapValuesDeep(val, callback));
+//     } else {
+//       return callback(value);
+//     }
+// };
 
 
-}
 
-exports.transmuteToProxy = function (unknown) {
-    return unknown.map(item => {
-        //Get the original tree item and replace the proxy with it
-        if (isEntity(value)) {
-            return  item[proxyObjSym]; //Every tree entity carries its proxy on this symbol keyed property
+//Should copy object and properties with prototype. Transforming values with the callback
+mapValuesDeep = (value, cbs={}) => {
+    if (Array.isArray(value)) {
+        const mappedArray = value.map(innerValue => mapValuesDeep(innerValue, cbs));
+        if (cbs.arrayCallback) {
+            return cbs.arrayCallback(mappedArray);
         } else {
-            return item;
+            return mappedArray;
         }
-    });
+    } else if (_.isObject(value)) {
+        const proto = Object.getPrototypeOf( value );
+        const mappedObj = Object.create(proto );
+
+        //Copy mapped properties to copied object
+        Object.assign(mappedObj,  _.mapValues(value, val => mapValuesDeep(val, cbs)) )
+        if (cbs.objCallback) {
+            return cbs.objCallback(mappedObj);
+        } else {
+            return mappedObj;
+        }
+    } else {
+        if (cbs.litCallback) {
+            return cbs.litCallback(value);
+        } else {
+            return value
+        }
+    }
+};
+
+
+
+
+
+module.exports = {
+    mapValuesDeep
 }
